7da044a49fde95ae21233910305d8ec0250567af
sahil-2211 <sahil.sh2.08.2004@gamil.com>
2024-02-01 01:05:18 +0530

    update

--- script.js
+++ script.js
@@ -5,24 +5,25 @@ let link=document.getElementById('link');
 let btnLogin=document.getElementById('btn');
 
 btnLogin.addEventListener('click',()=>{
-    let email=inputEmail.value;
-    let password=inputPassword.value;
-    if(email==''||password==''){
+    let Email=inputEmail.value;
+    let Password=inputPassword.value;
+    if(Email==''||Password==''){
         alert('Please fill all the fields');
     }
     else{
-        if(email=='sahilsh28004@gmail.com' && password=='sahilsh5831B'){
+        if(Email=='sahilsh28004@gmail.com' && Password=='sahilsh5831B'){
             alert('Login Successfull');
             inputEmail.value='';
             inputPassword.value='';
             window.location.href='index.html';
+            // loginForm.reset();
         }
         else{
             alert('Invalid Credentials');
             inputEmail.value='';
             inputPassword.value='';
+            loginForm.reset();
             window.location.href='index2.html';
-        
         }
     }
 })
